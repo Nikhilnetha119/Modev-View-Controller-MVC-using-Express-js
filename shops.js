f96bf2f9dcc8a1130764669e29c462b186bd9a22
Nikhil kumar <89766775+Nikhilnetha119@users.noreply.github.com>
2024-04-15 21:37:07 +0530

    Dynamic routing 2

--- shops.js
+++ shops.js
@@ -3,3 +3,14 @@ const path = require('path');
 exports.controllergetshop = ((req, res, next) => {
     res.sendFile(path.join(__dirname, '../', 'views' , 'shop.html'))
 })
+
+exports.postcart = ((req, res, next) => {
+    const productId = req.body.productId;
+    console.log(productId);
+    res.redirect('/success');
+})
+
+exports.getproduct = ((req, res, next) => {
+    const productId = req.params.productId;
+    console.log(productId);
+})
